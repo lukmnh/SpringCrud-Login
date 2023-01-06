0f144bf189cd12521ec76043f6c4c03cf9435a4e
lukmnh <86191995+lukmnh@users.noreply.github.com>
2023-01-06 17:01:44 +0700

    Update DivisionController.java
    update file

--- src/main/java/com/example/demo/controllers/DivisionController.java
+++ src/main/java/com/example/demo/controllers/DivisionController.java
@@ -3,6 +3,7 @@ package com.example.demo.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,13 +11,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.example.demo.daos.DivisionDao;
 import com.example.demo.models.Division;
 import com.example.demo.tools.DBConnection;
-
 import io.micrometer.core.lang.Nullable;
 
 @Controller
 @RequestMapping("division")
 public class DivisionController {
     DivisionDao div = new DivisionDao(DBConnection.getConnection());
+
+    // @ModelAttribute("divisionList")
+    // public String getRegion(Model model) {
+    // model.addAttribute("regName", div.getAllRegion());
+    // return "division/form";
+    // }
 
     @GetMapping()
     public String index(Model model) {
@@ -28,17 +34,9 @@ public class DivisionController {
     @GetMapping(value = { "form", "form/{id}" })
     public String form(@PathVariable(required = false) Integer Id, Model model) {
         if (Id != null) {
-            // get data by id for update
-            div.getById(Id);
-            Division division = new Division();
-            division.setId(1);
-            division.setName("Helper");
             model.addAttribute("division", div.getById(Id));
         } else {
-            // insert data for new data
-            Division division = new Division();
-            division.setId(5);
-            division.setName("Chef");
+            model.addAttribute("division", div.getRegion());
             model.addAttribute("division", new Division());
         }
         return "division/form";
@@ -55,19 +53,21 @@ public class DivisionController {
         }
 
         if (result) {
-            return "redirect:/region";
+            return "redirect:/division";
         } else {
-            return "region/form";
+            return "division/form";
         }
     }
 
     // DELETE
     @PostMapping(value = "/delete/{id}")
     public String delete(@PathVariable Integer id) {
-
-        Boolean result = div.deleteData(1);
+        // Division division = new Division();
+        id = 4;
+        // id = division.setId(id);
+        Boolean result = div.deleteData(id);
         if (result) {
-            return "redirect:/region";
+            return "redirect:/division";
         } else {
             return "";
         }
